879d2919d32580d04dd7b2553d1dd3ce3b193089
joaquinalvarezderose-hue <joaquinalvarezderose@gmail.com>
2026-06-21 17:14:12 -0300

    Update SponsorBanner.tsx

--- components/SponsorBanner.tsx
+++ components/SponsorBanner.tsx
@@ -7,9 +7,9 @@ interface Sponsor {
 }
 
 const sponsors: Sponsor[] = [
-  { src: '/images/sponsors/sponsor-1.png', alt: 'Sponsor 1' },
-  { src: '/images/sponsors/sponsor-2.png', alt: 'Sponsor 2' },
-  { src: '/images/sponsors/sponsor-3.png', alt: 'Sponsor 3' },
+  { src: '/images/sponsors/sponsor-1.jpeg', alt: 'Sponsor 1' },
+  { src: '/images/sponsors/sponsor-2.jpeg', alt: 'Sponsor 2' },
+  { src: '/images/sponsors/sponsor-3.jpeg', alt: 'Sponsor 3' },
 ];
 
 const SponsorBanner: React.FC = () => {
