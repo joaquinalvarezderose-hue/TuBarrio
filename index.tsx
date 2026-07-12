3173d42bc829498c1614a88f171f1d87dbcfdc9b
joaquinalvarezderose-hue <joaquinalvarezderose@gmail.com>
2026-07-12 14:48:46 -0300

    Add module-level logging to index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,15 @@
+console.log('[index.tsx] Loading...');
+
 import './tailwind.css';
 import 'material-symbols/outlined.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log('[index.tsx] Imports complete, finding root...');
+
 const rootElement = document.getElementById('root');
+console.log('[index.tsx] Root element:', rootElement);
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
